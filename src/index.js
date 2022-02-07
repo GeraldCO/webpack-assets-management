3f4af736509746e5acac72774be5268478d67d86
gerald cerda <geraldcerd@gmail.com>
2022-02-06 21:38:53 -0600

    commit before htmlWebpackPlugin

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import Notes from './data.csv';
 import toml from './data.toml';
 import yaml from './data.yaml';
 import json from './data.json5';
+import myName from './myName';
+import printMe from './print.js';
 
 console.log(toml.title); // output `TOML Example`
 console.log(toml.owner.name); // output `Tom Preston-Werner`
@@ -18,12 +20,18 @@ console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function component(){
     const element = document.createElement('div');
+    const btn = document.createElement('button');
     //Lodash now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.textContent = myName('Cody');
     element.classList.add('hello');
 
-    const
-    myIcon = new Image();
+    btn.innerHTML = 'Clicgit k me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+
+    const myIcon = new Image();
     myIcon.src = Icon;
 
     //element.appendChild(myIcon);
